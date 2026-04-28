1f6ece37c9d6972bbdcfca3846b98c442e5ec68f
moni patel <patidarmoni41@gmail.com>
2026-04-28 18:14:01 +0530

    ..

--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,6 +1,6 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authReducer from "../features/auth/authSlice";
-import adminReducer from "../features/admin/adminSlice";
+import adminReducer from "../features/admin/AdminSlice";
 import noteReducer from "../features/notes/noteSlice";
 import commentReducer from "../features/comments/commentSlice";
 import ratingReducer from "../features/rating/ratingSlice";
--- client/src/features/admin/AdminSlice.js
+++ client/src/features/admin/AdminSlice.js
@@ -1,5 +1,5 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import adminService from './adminService';
+import adminService from './AdminService';
 
 const initialState = {
   stats: null,
